08e867c95976a2fc9056eb9c20846884e4f682e9
HKLCF <hklcfs@gmail.com>
2021-07-08 18:52:56 +0800

    Create fix.bat

---
+++ fix.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+SET SPOTIFY_PATH=%AppData%\Spotify
+
+CD %SPOTIFY_PATH%\locales
+
+IF NOT EXIST zh-TW.mo (
+  MOVE zh-Hant.mo zh-TW.mo > nul
+  ECHO Fixed! Please restart Spotify.
+)
+
+ECHO Nothing to fix here.
+ECHO.
+
+PAUSE
